6e13e6fa6b6892c43e0d5896708b35a54efa5b18
RosieOh <20172207@gm.hannam.ac.kr>
2023-08-24 13:14:25 +0900

    2차 프로젝트 세팅

---
+++ src/model/DBConnect.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public interface DBConnect {
+    final static String NOTICE_SELECT_ALL = "select * from notice order by no desc";
+    final static String NOTICE_SELECT_ONE = "select * from notice where no=?";
+    final static String NOTICE_INSERT = "insert into notice values (default,?,?,default,default);";
+    final static String NOTICE_UPDATE = "update notice set title=?, content=? where no=?";
+    final static String NOTICE_DELETE = "delete from notice where no=?";
+
+    final static String CUSTOM_SELECT_ALL = "select * from custom order by regdate desc";
+    final static String CUSTOM_SELECT_ONE = "select * from custom where id=?";
+    final static String CUSTOM_SELECT_LOG = "select * from custom where id=?";
+    final static String CUSTOM_INSERT = "insert into custom values (?,?,?,default,default,?,?,?,default,?)";
+    final static String CUSTOM_UPDATE = "update custom set pw=?,tel=?,email=? where id=?";
+    final static String CUSTOM_DELETE = "delete from custom where id=?";
+
+    final static String PRODUCT_SELECT_ALL = "select * from product order by pno";
+    final static String PRODUCT_SELECT_ONE = "select * from product where pno=?";
+    final static String PRODUCT_INSERT = "insert into product values(default, ?, '', ?, ?, ?, ?, ?, ?, ?, ?)";
+    final static String PRODUCT_INSERT_UPDATE = "update product set prono = concat(cate, pno) where pno=?";
+    final static String PRODUCT_UPDATE = "update product set pname=?, cate=?, pcomment=?, plist, price=? imgsrc1=?, imgsrc2=?, imgsrc3=? where pno=?";
+    final static String PRODUCT_DELETE = "delete from product where pno=?";
+
+    // 입고 처리 패턴
+    final static String PRODUCT_RECEIVE = "insert into receive values (?, ?, ?)";
+    final static String CATEGORY_LOAD = "select * from category";
+
+    // 출고 처리 패턴
+    final static String SERVE_PAYMENT = "insert into payment values (default, ?, ?, ?, ?, ?, ?, ?, '')";
+    final static String SERVE_INSERT = "insert into serve values (default, ?, ?, ?, default)";
+    final static String DELIVERY_INSERT = "insert into delivery values (default, ?, ?, ?, ?, '', '', default, default, '', '')";
+    final static String CART_DELETE = "delete from cartno=?";
+
+    // 반품 처리 패턴
+    final static String RECEIVE_PAYMENT = "delete from payment where sno=?";
+    final static String RETURN_RECEIVE = "insert into receive values (default, ?, ?, ?, default)";
+    final static String RETURN_SERVE = "delete from serve where sno=?";
+    final static String RETURN_CART = "insert into cart values (default, ?, ?, ?)";
+    final static String RETURN_DELIVERY = "delete from delivery where sno=?";
+
+    // 배송 등록 처리
+    final static String DELIVERY_PRO = "update delivery set pcom=?, ptel=?, pstate=1, sdate=current_timestamp, rdate=?, bcode=? where dno=?;\n";
+    final static String DELIVERY_COMPLETE = "update delivery set pstate=2, where dno=?;";
+    final static String INVENTORY_SELECT_ONE = "select * from delivery;";
+    public Connection connect();
+    public void close(PreparedStatement pstmt, Connection conn);
+    public void close(ResultSet rs, PreparedStatement pstmt, Connection conn);
+}
