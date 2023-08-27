f52ac2cc53b9d7b69a7e1a5eb31453883dc17b56
emssme <bk75121@gmail.com>
2023-08-28 00:08:27 +0900

    파일업로드 에러

--- src/com/shop/model/DBConnect.java
+++ src/com/shop/model/DBConnect.java
@@ -26,14 +26,27 @@ public interface DBConnect {
 
     //상품 입고
     final static String PRODUCT_INSERT = "insert into product values(default, ?, '', ?, ?, ?, ?, ?, ?, default)";
+    final static String CATEGORY_LOAD = "select * from category";
 
     //상품번호+상품카테고리 pro_cate_no
-    final static String PRODUCT_INSERT_UPDATE = "update product set pro_no = concat(cate_id, pro_no) where pro_no in (select pro_no from product order by regdate desc limit 1)";
+    final static String PRODUCT_INSERT_UPDATE = "update product set pro_cate_no = concat(cate_id,'-',pro_no) where pro_no in (select pro_no from product order by regdate desc limit 1)";
+
+
+    //상품 추가 정보
+    final static String PRODUCT_INFO = "insert into addinfo values(default, ?, ?, ?, default)";
+
 
     //상품 수정
     final static String PRODUCT_UPDATE = "update product set price=?, title=?, description=?, pro_content=?, thumb=?, img_src=? where pro_no=?";
 
     //상품 삭제
     final static String PRODUCT_DELETE = "delete from product where pro_no=?";
+
+    //재고 조회
+    final static String INVENTORY_SELECT_ALL = "select * from inventory order by pro_no desc";
+    final static String INVENTORY_SELECT_ONE = "select * from inventory where pro_no=?";
+
+
+
 
 }
--- web/product/productList.jsp
+++ web/product/productList.jsp
@@ -68,15 +68,15 @@
 <!-- Hero Section End -->
 
 <!-- Breadcrumb Section Begin -->
-<section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+<section class="breadcrumb-section set-bg" style="background-image: url("${path}/img/breadcrumb.jpg")>
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <div class="breadcrumb__text">
                     <h2>Organi Shop</h2>
                     <div class="breadcrumb__option">
-                        <a href="../index.html">Home</a>
-                        <span>Shop</span>
+                        <a href="${path}">Home</a>
+                        <span>상품 목록</span>
                     </div>
                 </div>
             </div>
